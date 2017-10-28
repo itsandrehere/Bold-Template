7a5fd38abc877c96311fb03c71db8c69576e0038
Andrea Guevara <andregueva18@gmail.com>
2017-10-28 15:31:09 -0400

    v 0.2

--- resources/views/app/sidebar.blade.php
+++ resources/views/app/sidebar.blade.php
@@ -3,11 +3,11 @@
 				<h1><a href="/">Bold<span>.</span></a></h1>
 				<nav role="navigation">
 					<ul>
-						<li><a href="/productos">Products</a></li>
-						<li><a href="/servicios">Services</a></li>
-						<li><a href="/precios">Pricing</a></li>
-						<li><a href="/acercade">About</a></li>
-						<li><a href="/contacto">Contact</a></li>
+						<li class="{{ Active::check('productos') }}"><a href="/productos">Products</a></li>
+						<li class="{{ Active::check('servicios') }}"><a href="/servicios">Services</a></li>
+						<li class="{{ Active::check('precios') }}"><a href="/precios">Pricing</a></li>
+						<li class="{{ Active::check('acercade') }}"><a href="/acercade">About</a></li>
+						<li class="{{ Active::check('contacto') }}"><a href="/contacto">Contact</a></li>
 						<li class="cta"><a href="#">Get started</a></li>
 					</ul>
 				</nav>
